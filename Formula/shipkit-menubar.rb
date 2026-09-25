db4f5d4589a6229e9d3f3bac12ab5a723b0e03b9
toygunchill <85456096+toygunchill@users.noreply.github.com>
2026-09-25 11:23:49 +0300

    feat(formula): 0.1.11

--- Formula/shipkit-menubar.rb
+++ Formula/shipkit-menubar.rb
@@ -15,8 +15,8 @@ class ShipkitMenubar < Formula
   desc "Menu-bar approval and review surface for shipkit"
   homepage "https://github.com/toygunchill/shipkit"
   url "https://github.com/toygunchill/shipkit.git",
-      tag:      "v0.1.10",
-      revision: "74e55157ee314307d5bd7e0f0d89b2a4b5721d86"
+      tag:      "v0.1.11",
+      revision: "a8b37b02ae2bf0d7018524eb49e2eec647dead92"
   license "MIT"
   head "https://github.com/toygunchill/shipkit.git", branch: "main"
 
